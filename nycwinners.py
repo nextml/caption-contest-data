c04c89d7471bfd0336c0568a0db2d0b6d2f9ad45
Scott <stsievert@users.noreply.github.com>
2021-01-19 18:58:46 -0600

    Initial commit

---
+++ nycwinners.py
@@ -0,0 +1,43 @@
+import json
+from typing import List, Dict
+import pandas as pd
+
+def _get_nyc_winners():
+    """
+    This file obtained by running this query at [1]:
+
+        query {   cartoons(first: 400000) {
+        id,
+        title,
+        announceFinalistsDate
+        contestFinalists {
+         id,
+         text,
+         rating
+        }
+        } }
+
+    [1]:https://graphql.newyorker.com/graphiql
+    """
+    with open("raw.json", "r") as f:
+        return json.load(f)
+
+def _expand(contest):
+    winners = contest.pop("contestFinalists")
+    out = {}
+    for winner in winners:
+        out[f"rank{winner['rating']}"] = winner["text"]
+    out.update(contest)
+    return out
+
+
+if __name__ == "__main__":
+    raw = _get_nyc_winners()
+    mrare = filter(lambda c: "contest" in c["title"].lower(), raw["data"]["cartoons"])
+    medium = [_expand(d) for d in mrare]
+    cooked = [m for m in medium if any("rank" in k for k in m.keys())]
+    keys = [set(d.keys()) for d in cooked]
+    assert all(keys[0] == k for k in keys), "All contests should have a top-rated caption"
+
+    with open("nyc_winners.json", "w") as f:
+        json.dump(cooked, f)
