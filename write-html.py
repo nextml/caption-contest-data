320082e422ce9aa50f4b9ded7c78f486113572b4
Scott Sievert <stsievert@users.noreply.github.com>
2022-07-10 23:50:37 -0400

    Include contests 510–659 (#2)

--- write-html.py
+++ write-html.py
@@ -3,28 +3,54 @@ from typing import Dict
 from pathlib import Path
 from typing import List, Optional
 from datetime import datetime, timedelta
+from collections import OrderedDict
 
+import yaml
+from toolz import groupby
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
 
-def _get_html_for_contest(contest: int, template, winners=None, dates=None):
-    summary = pd.read_csv(Path("summaries") / f"{contest}.csv")
-    captions = [{"rank": k, **row} for k, row in summary.iterrows()]
+def _get_html_for_contest(
+        summaries,#OrderedDict[str, pd.DataFrame],
+        contest: int,
+        template,
+        winners=None,
+        meta=None
+):
+    # captions = [{"rank": k, **row} for k, row in summary.iterrows()]
+    samplers = list(summaries.keys())
+    for summary in summaries.values():
+        if "score" in summary.columns and "mean" not in summary.columns:
+            summary["mean"] = summary["score"]
+    captions = {
+        sampler: [{"rank": k, **row} for k, row in summary.iterrows()]
+        for sampler, summary in summaries.items()
+    }
 
     out = template.render(
         captions=captions,
         contest=contest,
         cartoon=f"cartoons/{contest}.jpg",
         winners=winners,
-        dates=dates,
+        meta=meta,
+        samplers=samplers,
+        summary_fnames=[f.name for f in Path("summaries").glob(f"{contest}*.csv")],
     )
     return out
 
 
 def _get_winner(contest: int) -> str:
-    f = Path("summaries") / f"{contest}.csv"
+    summaries = Path("summaries")
+    _files = list(summaries.glob(f"{contest}*.csv"))
+    if len(_files) > 1:
+        files = [f for f in _files if "UCB" in f.name]
+        f = files[0]
+    else:
+        f = _files[0]
     df = pd.read_csv(f)
+    if "mean" not in df.columns:
+        df["mean"] = df["score"]
     idx = df["mean"].idxmax()
     return str(df.loc[idx, "caption"])
 
@@ -69,6 +95,12 @@ if __name__ == "__main__":
             for k, v in mwell.items()
             if "Contest #" in k
         }
+    for contest, v in meta.items():
+        summaries = Path()
+        dfs = [pd.read_csv(f) for f in Path("summaries").glob(f"{contest}*.csv")]
+        v["n_captions"] = int(max({len(df) for df in dfs}))
+        v["n_responses"] = int(sum(df["votes"].sum() for df in dfs))
+
         #  "votingEndDate": "2019-07-21T22:44:00.000Z",
         #  "announceFinalistsDate": "2019-07-15T22:42:00.000Z",
         #  "contestSubmissionEndDate": "2019-06-30T22:42:00.000Z",
@@ -77,15 +109,20 @@ if __name__ == "__main__":
 
     nycc_winners.update(chosen)
 
-    contests = {int(f.name.replace(".csv", "")) for f in summaries.glob("*.csv")}
+    def _get_contest(fname: str) -> int:
+        rare = fname.replace(".csv", "")
+        mrares = rare.split("_")
+        return int(mrares[0])
+
+    summaries_dir = Path("summaries")
+    contests = {_get_contest(f.name) for f in summaries_dir.glob("*.csv")}
 
     # contest, cartoon, winner
-    summary = [
+    _summary = [
         {"contest": k, "cartoon": f"cartoons/{k}.jpg", "winner": _get_winner(k)}
         for k in contests
     ]
-
-    summary = list(sorted(summary, key=lambda x: -x["contest"]))
+    summary = list(sorted(_summary, key=lambda x: -x["contest"]))
 
     def _get_end_date(v: Dict[str, str]) -> Optional[datetime]:
         if "votingEndDate" not in v or v["votingEndDate"] is None:
@@ -101,18 +138,64 @@ if __name__ == "__main__":
         for k, v in meta.items()
         if _get_end_date(v) and _get_end_date(v) <= datetime.now()
     }
-    summary = [
-            v 
-            for v in summary 
 
-            if v["contest"] in meta2 and _get_end_date(meta2[v["contest"]])
-                and _get_end_date(meta2[v["contest"]]) <= datetime.now()]
+    summary = [
+        v
+        for v in summary
+        if v["contest"] < 620 or (
+            v["contest"] in meta2
+            and _get_end_date(meta2[v["contest"]]) <= datetime.now()
+        )
+    ]
+
+    def _get_sampler(x: str) -> str:
+        y = x.replace(".csv", "").replace("summary", "")
+        if y[:3].isdigit():
+            y = y[3:]
+        if y == "":
+            return "lil-KLUCB"
+        return y.replace("_", "")
+    def _get_contest(x: str) -> int:
+        assert x[:3].isdigit()
+        return int(x[:3])
+
+    summaries_dir = Path("summaries")
+    fnames = [f.name for f in summaries_dir.glob(f"*.csv")]
+    contest_fnames = groupby(_get_contest, fnames)
+    contest_samplers = {
+            contest: [_get_sampler(f) for f in fnames]
+            for contest, fnames in contest_fnames.items()
+    }
+    samplers_html = {c: ", ".join(s) for c, s in contest_samplers.items()}
+
+
+    with open(Path("io") / "info-510-thru-659.yaml", "r") as f:
+        old_info = yaml.safe_load(f)
+    for v in old_info.values():
+        y, m, d = v["votingStartDate"].split("-")
+        start = datetime(int(y), int(m), int(d))
+        end = start + timedelta(days=13)
+        v["announceFinalistsIssueDate"] = end.isoformat()[:10] + " (estimated)"
+    assert set(meta.keys()).intersection(set(old_info.keys())) == set()
+    meta.update(old_info)
+    meta2.update(old_info)
 
     out = env.get_template("index.html").render(
-        summary=summary, nycc_winners=nycc_winners, dates=meta2
+        summary=summary, nycc_winners=nycc_winners, dates=meta2, samplers=samplers_html,
     )
     with open(f"index.html", "w") as fh:
         fh.write(out)
+
+    for contest in list(contests)[::-1]:
+        if contest % 10 == 0:
+            print(contest)
+
+        fnames = sorted([f for f in summaries_dir.glob(f"{contest}*.csv")])
+        summaries = OrderedDict([(_get_sampler(f.name), pd.read_csv(f)) for f in fnames])
+        print(summaries.keys())
+        out = _get_html_for_contest(summaries, contest, template, nycc_winners.get(contest, []), meta=meta.get(contest, {}))
+        with open(f"dashboards/{contest}.html", "w") as fh:
+            fh.write(out)
 
     #  nyc_winn
     ## Cartoons are about 672KB each -> 80 of them are 52.5MB.
@@ -127,10 +210,3 @@ if __name__ == "__main__":
     #  tf.add(f"cartoons/{f.name}")
     #  for f in Path("summaries").glob("*.csv"):
     #  tf.add(f"summaries/{f.name}")
-
-    for contest in list(contests)[::-1]:
-        if contest % 10 == 0:
-            print(contest)
-        out = _get_html_for_contest(contest, template, nycc_winners.get(contest, []), dates=meta.get(contest, {}))
-        with open(f"dashboards/{contest}.html", "w") as fh:
-            fh.write(out)
